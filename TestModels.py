662ba98c529eb898c0c7e8188bf81cc9222ca214
SachinJay <sjd232@cornell.edu>
2022-04-06 11:39:21 -0400

    Updated tests

--- TestModels.py
+++ TestModels.py
@@ -1,4 +1,9 @@
 from AlexNet import AlexNet
 from ResNet import ResNet
+from VGG import VGG16, VGG19
 
-alex = AlexNet((227,227,3), 10)
+# alex = AlexNet((227,227,3), 10)
+
+vgg16 = VGG16((32,32,3), 10)
+vgg19 = VGG19((32,32,3),10)
+
