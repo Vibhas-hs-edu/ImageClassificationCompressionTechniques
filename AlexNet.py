ce78936059d8e926be7dac633385a40e9b7cb245
SachinJay <sjd232@cornell.edu>
2022-04-06 11:10:58 -0400

    Added AlexNet Model
    NOTE:
    -Takes in any input_shape but resizes to (224,224,3)

--- AlexNet.py
+++ AlexNet.py
@@ -1,2 +1,31 @@
-import tensorflow
+import tensorflow as tf
 import keras
+from tensorflow.keras import datasets, layers, models, losses
+
+class AlexNet:
+    def __init__(self, input_shape, num_classes) -> None:
+        model = keras.models.Sequential()
+        model.add(layers.experimental.preprocessing.Resizing(224, 224, interpolation="bilinear", input_shape=input_shape))
+        model.add(layers.Conv2D(96, 11, strides=4, padding='same'))
+        model.add(layers.Lambda(tf.nn.local_response_normalization))
+        model.add(layers.Activation('relu'))
+        model.add(layers.MaxPooling2D(3, strides=2))
+        model.add(layers.Conv2D(256, 5, strides=4, padding='same'))
+        model.add(layers.Lambda(tf.nn.local_response_normalization))
+        model.add(layers.Activation('relu'))
+        model.add(layers.MaxPooling2D(3, strides=2))
+        model.add(layers.Conv2D(384, 3, strides=4, padding='same'))
+        model.add(layers.Activation('relu'))
+        model.add(layers.Conv2D(384, 3, strides=4, padding='same'))
+        model.add(layers.Activation('relu'))
+        model.add(layers.Conv2D(256, 3, strides=4, padding='same'))
+        model.add(layers.Activation('relu'))
+        model.add(layers.Flatten())
+        model.add(layers.Dense(4096, activation='relu'))
+        model.add(layers.Dropout(0.5))
+        model.add(layers.Dense(4096, activation='relu'))
+        model.add(layers.Dropout(0.5))
+        model.add(layers.Dense(10, activation='softmax'))
+
+        self.model = model
+        self.model.compile()
