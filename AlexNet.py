58247db40e07ff3cedf76cca346552fee43ddb07
SachinJay <sjd232@cornell.edu>
2022-04-06 12:39:20 -0400

    Added docstring to AlexNet

--- AlexNet.py
+++ AlexNet.py
@@ -6,6 +6,23 @@ from tensorflow.keras import datasets, layers, models, losses
 
 class AlexNet:
     def __init__(self, input_shape, num_classes) -> None:
+        """
+        Class to abstract away implementation of AlexNet
+
+        Parameters:
+        ------------- 
+        input_shape (3-tuple): Shape of the input. Must have 3 channels and be 
+        at least 32x32. For example, 32x32x3 is valid, 240x240x3 is valid,
+        5x5x3 is not valid. NOTE: all inputs are reshaped to 224x224x3 to be 
+        compatible with AlexNet architecture 
+
+        num_classes (int): number of output classes there are in the data
+
+        Attributes:
+        --------------
+        model: the AlexNet model
+
+        """
         model = keras.models.Sequential()
         model.add(layers.experimental.preprocessing.Resizing(224, 224, interpolation="bilinear", input_shape=input_shape))
         model.add(layers.Conv2D(96, 11, strides=4, padding='same'))
