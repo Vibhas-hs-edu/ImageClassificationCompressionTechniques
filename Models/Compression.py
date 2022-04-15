a4e9e7466cd3a149a93719df84494dfbe34eaf68
SachinJay <sjd232@cornell.edu>
2022-04-15 12:59:42 -0400

    Added class for pruning

--- Models/Compression.py
+++ Models/Compression.py
@@ -1,8 +1,78 @@
 # Class for taking in TRAINED TENSORFLOW models and outputting a clustered model
 # Weights are clustered according to the paper, network is then finetuned
 
+from gc import callbacks
 import tensorflow_model_optimization as tfmot
 import tensorflow as tf
+import numpy as np
+
+class Prune:
+    def __init__(self, trained_model) -> None:
+        """
+        Class to abstract away Pruning functionality
+
+        Takes in trained tensorflow model and performs pruning on it to reduce 
+        space
+
+        Params:
+        ---------------
+        trained_model: a trained tensorflow model.E.g. ResNet-20 trained on 
+        CIFAR-10
+
+        history: history of the model after fine tuning occurs
+
+        Usage:
+        ------------
+        Construct an instance with a pretrained tensorflow model. Then call 
+        prune()
+        """
+
+        self.model = trained_model
+    
+    def prune(self,train_images, train_labels,batch_size=128,fine_tune_epochs=2, final_sparsity=0.8):
+
+        """
+        Prunes the network to the specified sparsity
+
+        Params:
+        --------
+
+        train_images: images to train on
+
+        train_labels: one-hot label vectors
+
+        final_sparsity: the final sparsity we want to achieve for the network, 
+        i.e., what percent of the connections do we want pruned
+        """
+
+
+        prune_low_magnitude = tfmot.sparsity.keras.prune_low_magnitude
+
+        num_images = train_images.shape[0]
+        end_step = np.ceil(num_images / batch_size).astype(np.int32) * fine_tune_epochs
+
+        # Define model for pruning.
+        pruning_params = {
+            'pruning_schedule': tfmot.sparsity.keras.PolynomialDecay(initial_sparsity=0.50,
+                                                                    final_sparsity=final_sparsity,
+                                                                    begin_step=0,
+                                                                    end_step=end_step)
+        }
+
+        model_for_pruning = prune_low_magnitude(self.model, **pruning_params)
+
+        model_for_pruning.compile(optimizer='adam',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+        callbacks = [tfmot.sparsity.keras.UpdatePruningStep()]
+
+        model_for_pruning.fit(train_images,train_labels,batch_size=batch_size,epochs=fine_tune_epochs,callbacks=callbacks)
+
+        self.model = model_for_pruning
+        
+
+        
 
 class Cluster:
     def __init__(self, trained_model) -> None:
@@ -17,12 +87,11 @@ class Cluster:
         trained_model: a trained tensorflow model. E.g. ResNet-20 
         trained on CIFAR-10
 
-        num_clusters: the number of unique weights we want in the network
-
         Fields:
         ------------
         model: a tensorflow model. This model will be affected by calls to 
         cluster() and finetune()
+
         history: history of the model after fine tuning occurs
 
         Usage:
