ad6a5a6a10dd136e78a1657d37109320a475e078
Vibhas-hs-edu <vn2302@columbia.edu>
2022-04-18 09:14:56 -0400

    Main script which runs the training loop

---
+++ main.py
@@ -0,0 +1,20 @@
+from train.train import train_model
+from Data.prepare_data import get_generator
+from Models.AlexNet import AlexNet
+from Models.ResNet import ResNet
+import tensorflow as tf
+
+input_shape = (512, 512, 3)
+num_classes = 3
+depth = 20
+
+resnet_model = ResNet(input_shape = input_shape, depth = 20, num_classes = num_classes)
+alexnet_model = AlexNet(input_shape = input_shape, num_classes = num_classes)
+
+train_generator = get_generator("Train", input_shape)
+val_generator = get_generator("Val", input_shape)
+test_generator = get_generator("Test", input_shape)
+
+loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits = False)
+
+train_model(train_dataset = train_generator, val_dataset = val_generator, model = alexnet_model, loss_fn = loss_fn)
